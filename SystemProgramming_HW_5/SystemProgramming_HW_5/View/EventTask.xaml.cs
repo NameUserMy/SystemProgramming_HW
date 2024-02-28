868526dd454150df03901e0f1f333addb5e1d859
unknown <My@example.com>
2024-02-28 09:47:49 +0200

    SystemProgramming_HW_5

---
+++ SystemProgramming_HW_5/SystemProgramming_HW_5/View/EventTask.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SystemProgramming_HW_5.Control;
+
+namespace SystemProgramming_HW_5.View
+{
+    /// <summary>
+    /// Interaction logic for EventTask.xaml
+    /// </summary>
+    public partial class EventTask : UserControl
+    {
+        EventTaskThreadControl controlEvent;
+        public EventTask()
+        {
+            InitializeComponent();
+            controlEvent = new EventTaskThreadControl();
+            this.DataContext = controlEvent;
+        }
+        private void StartAutoResetEvent_Click(object sender, RoutedEventArgs e)
+        {
+            controlEvent.StartAutoResetEvent();
+        }
+
+        
+    }
+}
---
+++ SystemProgramming_HW_5/SystemProgramming_HW_5/View/Start.xaml.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Windows;
+using SystemProgramming_HW_5.Control;
+
+
+namespace SystemProgramming_HW_5.View
+{
+    /// <summary>
+    /// Interaction logic for Start.xaml
+    /// </summary>
+    public partial class Start : Window
+    {
+        StartControl startControl;
+      
+        public Start()
+        {
+            startControl=new StartControl(this);
+            startControl.Max3();
+            InitializeComponent();
+           
+        }
+
+        private void secondBooton_Click(object sender, RoutedEventArgs e)
+        {
+            CC.Content = new StopwatchTest();
+        }
+        private void firstBooton_Click(object sender, RoutedEventArgs e)
+        {
+            CC.Content = new EventTask();
+        }
+        private void Fourth_Click(object sender, RoutedEventArgs e)
+        {
+           CC.Content = new MutexView();
+        }
+        private void newApp_Click(object sender, RoutedEventArgs e)
+        {
+            new  Start().Show();
+        }
+
+        private void semafor_Click(object sender, RoutedEventArgs e)
+        {
+            CC.Content = new SemaforView();
+        }
+    }
+}
